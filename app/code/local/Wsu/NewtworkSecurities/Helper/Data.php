50d0af5b56eb0266bab6c74fed627b297136e37e
jeremyBass <jeremybass@cableone.net>
2013-11-08 13:54:57 -0800

    added a require customer login for frontend
    for now it has a regex for a white list, but think it may be better to
    build it off a selected list?

--- app/code/local/Wsu/NewtworkSecurities/Helper/Data.php
+++ app/code/local/Wsu/NewtworkSecurities/Helper/Data.php
@@ -18,7 +18,12 @@ class Wsu_NewtworkSecurities_Helper_Data extends Mage_Core_Helper_Abstract {
         }
         Mage::log($data, null, 'wsu-newtworksecurities.log');
     }
-
+    public function isLoginRequired($store = null) {
+        return Mage::getStoreConfigFlag('wsu_newtworksecurities/startup/require_login', $store);
+    }
+    public function getWhitelist($store = null) {
+        return Mage::getStoreConfig('wsu_newtworksecurities/startup/require_login_whitelist', $store);
+    }
 	/**
      * Show newtworksecurities only after certain number of unsuccessful attempts
      */
---
+++ app/code/local/Wsu/NewtworkSecurities/Model/Customer/Observer.php
@@ -0,0 +1,20 @@
+<?php
+class Wsu_NewtworkSecurities_Model_Customer_Observer {
+    public function requireLogin($observer) {
+        $helper = Mage::helper('wsu_newtworksecurities');
+        $session = Mage::getSingleton('customer/session');
+
+        if (!$session->isLoggedIn() && $helper->isLoginRequired()) {
+            $controllerAction = $observer->getEvent()->getControllerAction();
+            /* @var $controllerAction Mage_Core_Controller_Front_Action */
+            $requestString = $controllerAction->getRequest()->getRequestString();
+
+            if (!preg_match($helper->getWhitelist(), $requestString)) {
+                $session->setBeforeAuthUrl($requestString);
+                $controllerAction->getResponse()->setRedirect(Mage::getUrl('customer/account/login'));
+                $controllerAction->getResponse()->sendResponse();
+                exit;
+            }
+        }
+    }
+}
