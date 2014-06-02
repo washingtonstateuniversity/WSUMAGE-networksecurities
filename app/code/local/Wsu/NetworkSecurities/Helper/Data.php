4e756e50d4730b8903321f83b15cce3b8302e1c2
jeremyBass <jeremybass@cableone.net>
2014-06-02 16:13:50 -0700

    moves the ip filter to after the honeypot
    we still want to try to log and filter those.. good for reporting later

--- app/code/local/Wsu/NetworkSecurities/Helper/Data.php
+++ app/code/local/Wsu/NetworkSecurities/Helper/Data.php
@@ -89,6 +89,20 @@ class Wsu_NetworkSecurities_Helper_Data extends Mage_Core_Helper_Abstract {
 		if ($usehoneypots){
 			$helper->testloginPots($username,$password);
 		}
+		
+		$use_ipfilter=Mage::getStoreConfig('wsu_networksecurities/genadmin_settings/use_ipfilter_admin');
+		
+		if($use_ipfilter==1){
+			$ip = $helper->get_ip_address();
+			$ipfilter=Mage::getStoreConfig('wsu_networksecurities/genadmin_settings/ipfilter_admin');
+			$mode=Mage::getStoreConfig('wsu_networksecurities/genadmin_settings/ipfiltermode_admin');			
+			$match=preg_match('/'.$ipfilter.'/',$ip);
+			if($match>0 && $mode==Wsu_NetworkSecurities_Helper_Data::IPMODE_EXCLUDE || $match==0 && $mode==Wsu_NetworkSecurities_Helper_Data::IPMODE_INCLUDE){
+				Mage::getSingleton('core/session')->addError('You are trying to access the admin from an unsecure connection.  You may need to VPN in.  Please contact your admin.');
+				return false;
+			}
+		}
+		
 		return true;
 	}
 	
