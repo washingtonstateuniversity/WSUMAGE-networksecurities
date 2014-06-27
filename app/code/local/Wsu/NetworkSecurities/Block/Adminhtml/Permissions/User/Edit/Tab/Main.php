a273d96e985825e64dba46a410871f145421fa00
jeremyBass <jeremybass@cableone.net>
2014-06-27 14:35:06 -0700

    set the correct id for the field set

--- app/code/local/Wsu/NetworkSecurities/Block/Adminhtml/Permissions/User/Edit/Tab/Main.php
+++ app/code/local/Wsu/NetworkSecurities/Block/Adminhtml/Permissions/User/Edit/Tab/Main.php
@@ -5,7 +5,7 @@ class Wsu_NetworkSecurities_Block_Adminhtml_Permissions_User_Edit_Tab_Main exten
 	  
         parent::_prepareForm();        
         $form = $this->getForm();
-        $fieldset = $form->getElements()->searchById('base_fieldset');
+        $fieldset = $form->getElements()->searchById('user_base_fieldset');
         
 	  
         $fieldset->addField('ldap_user', 'select', array(
