5bbd6e8c7b3d6fc8db06a3eef118f69d478baa9c
jeremyBass <jeremybass@cableone.net>
2014-07-01 08:15:32 -0700

    sets field to disabled and notes why

--- app/code/local/Wsu/NetworkSecurities/Block/Adminhtml/Permissions/User/Edit/Tab/Main.php
+++ app/code/local/Wsu/NetworkSecurities/Block/Adminhtml/Permissions/User/Edit/Tab/Main.php
@@ -17,8 +17,10 @@ class Wsu_NetworkSecurities_Block_Adminhtml_Permissions_User_Edit_Tab_Main exten
 			'id'        => 'is_active',
 			'title'     => Mage::helper('adminhtml')->__('LDAP user'),
 			'class'     => 'input-select',
+			'disabled'	=> true,
 			'style'     => 'width: 80px',
 			'options'   => array('1' => Mage::helper('adminhtml')->__('Yes'), '0' => Mage::helper('adminhtml')->__('No')),
+			'after_element_html' => '<br/><small>'.Mage::helper('adminhtml')->__('This field is automaticly detected if you log in with an AD account.  If you change the user name and it doesn\'t match an AD user, then this will move to no.').'</small>',
 		)); 
 		
 		return $this;
