7049eb7e095c007c9edcfadb727ee98fe05b69a2
Jeremy Bass <jeremybass@cableone.net>
2014-08-16 12:12:33 -0700

    adds a method to get the custom by email only

--- app/code/local/Wsu/Networksecurities/Helper/Customer.php
+++ app/code/local/Wsu/Networksecurities/Helper/Customer.php
@@ -1,4 +1,17 @@
 <?php
 class Wsu_Networksecurities_Helper_Customer extends Mage_Core_Helper_Abstract {
-
+	
+	public function getCustomerByEmail($email, $website_id=null){ //edit
+		$collection = Mage::getModel('customer/customer')->getCollection()
+					->addFieldToFilter('email', $email);
+		if (Mage::getStoreConfig('customer/account_share/scope') && $website_id!=null) {
+			$collection->addFieldToFilter('website_id',$website_id);
+		}
+		return $collection->getFirstItem();
+	}
+	
+	
+	
+	
+	
 }
