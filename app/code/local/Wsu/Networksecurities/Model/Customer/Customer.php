6ef31d51704816cbdcff6bb080d2f46877d5fd70
Jeremy Bass <jeremybass@cableone.net>
2014-08-16 12:38:51 -0700

    set up to check a username

--- app/code/local/Wsu/Networksecurities/Model/Customer/Customer.php
+++ app/code/local/Wsu/Networksecurities/Model/Customer/Customer.php
@@ -3,7 +3,7 @@ class Wsu_Networksecurities_Model_Customer_Customer extends Mage_Customer_Model_
     /**
      * Authenticate customer
      *
-     * @param  string $login
+     * @param  string $login email or username
      * @param  string $password
      * @throws Mage_Core_Exception
      * @return true
@@ -12,13 +12,19 @@ class Wsu_Networksecurities_Model_Customer_Customer extends Mage_Customer_Model_
     public function authenticate($username, $password){
 		//$login = trim(mb_convert_kana($login, 'as'));
 		$actived = trim(Mage::getStoreConfig('wsu_networksecurities/ldap/customerlogin/activeldap'));
-		if (!$actived) //CHECK MAGENTO CONNECT
+		if (!$actived){ //CHECK MAGENTO CONNECT
 		        if (!$this->validatePassword($password)) {
 					Mage::helper('wsu_networksecurities')->setFailedLogin($username,$password);
 				}
 				return parent::authenticate($username, $password);
+		}
+		
+		if(Zend_Validate::is($username, 'EmailAddress')){ 
+			$this->loadByEmail($username); 
+		}else if (Mage::getStoreConfigFlag('username/general/enabled')) { 
+			$this->loadByUsername($username);    
+		} 
 
-        $this->loadByEmail($username);
         if ($this->getConfirmation() && $this->isConfirmationRequired()) {
             throw Mage::exception('Mage_Core', Mage::helper('customer')->__('This account is not confirmed.'),
                 self::EXCEPTION_EMAIL_NOT_CONFIRMED
