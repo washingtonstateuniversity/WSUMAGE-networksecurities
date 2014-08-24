2d1c57a3e938f4441455883507ed20be752b8d82
Jeremy Bass <jeremybass@cableone.net>
2014-08-24 12:18:41 -0700

    remove the die

--- app/code/local/Wsu/Networksecurities/controllers/ErrorController.php
+++ app/code/local/Wsu/Networksecurities/controllers/ErrorController.php
@@ -2,7 +2,7 @@
 
 class Wsu_Networksecurities_ErrorController extends Mage_Core_Controller_Front_Action {
     public function indexAction() {
-		die("got to the error in _ErrorController");
+		//die("got to the error in _ErrorController");
         $this->loadLayout();
         $this->renderLayout();
     }
