1d288df26b366bbd606648b96fa448ea8bccc6be
Jeremy Bass <jeremybass@cableone.net>
2014-08-30 16:52:49 -0700

    sets up yahoo to get all info possible to the system

--- app/code/local/Wsu/Networksecurities/controllers/Sso/YaloginController.php
+++ app/code/local/Wsu/Networksecurities/controllers/Sso/YaloginController.php
@@ -14,19 +14,42 @@ class Wsu_Networksecurities_Sso_YaloginController extends Mage_Core_Controller_F
 			$session = $yalogin->getSession();
 			$userSession = $session->getSessionedUser();
 			$profile = $userSession->loadProfile();
+			$user = array();
 			
-			var_dump($profile);die();
+			
+			
 			
 			$emails = $profile->emails;
-			$user = array();
-			foreach($emails as $email) {
-				if($email->primary == 1){
-					$user['email'] = $email->handle;
+			if(isset($emails)){
+				foreach($emails as $email) {
+					if($email->primary == 1){
+						$user['email'] = $email->handle;
+					}
 				}
 			}
+			$user['username'] = $profile->nickname;
+			if(!isset($user['email'])){
+				$user['email']=$user['username'].'@yahoo.com';
+			}
+			
 			$user['firstname'] = $profile->givenName;
 			$user['lastname'] = $profile->familyName;
+			if(!isset($user['firstname'])){
+				$user['firstname']=$user['username'];
+			}
+			if(!isset($user['lastname'])){
+				$user['firstname']=$user['username'];
+			}
+			$gender = $profile->gender;
 			
+			if(isset($gender)){
+				$user['gender'] = $gender=="M" ? '1' : '2';
+			}
+			$birthYear = $profile->birthYear;
+			if(isset($birthYear)){
+				$user['dob'] = '1/1/'.$birthYear;
+			}
+			var_dump($user);die();
 			//get website_id and sote_id of each stores
 			$store_id = Mage::app()->getStore()->getStoreId();
 			$website_id = Mage::app()->getStore()->getWebsiteId();
