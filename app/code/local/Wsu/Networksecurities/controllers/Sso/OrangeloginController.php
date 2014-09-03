bf62a6696e8a165cd961303be6a3fba1efe7e78d
jeremyBass <jeremybass@cableone.net>
2014-09-03 08:41:54 -0700

    normalize the orange provider

--- app/code/local/Wsu/Networksecurities/controllers/Sso/OrangeloginController.php
+++ app/code/local/Wsu/Networksecurities/controllers/Sso/OrangeloginController.php
@@ -10,37 +10,27 @@ class Wsu_Networksecurities_Sso_OrangeloginController extends Mage_Core_Controll
 		$coreSession = Mage::getSingleton('core/session');                      
 		$user_info = $org->data;                 
 		if(count($user_info)) {
-			$frist_name = $user_info['openid_sreg_nickname'];
-			$last_name = $user_info['openid_sreg_nickname'];
-			$email = $user_info['openid_sreg_email'];                    
-			
-			//get website_id and sote_id of each stores
-			$store_id = Mage::app()->getStore()->getStoreId();//add
-			$website_id = Mage::app()->getStore()->getWebsiteId();//add
-			
-			$data = array('firstname'=>$frist_name, 'lastname'=>$last_name, 'email'=>$email);
-			
-			$customer = $customerHelper->getCustomerByEmail($data['email'],$website_id);
-			if(!$customer || !$customer->getId()) {
-				//Login multisite
-				$customer = $customerHelper->createCustomerMultiWebsite($data, $website_id, $store_id );
-				if (Mage::getStoreConfig('wsu_networksecurities/orangelogin/is_send_password_to_customer')) {
-					$customer->sendPasswordReminderEmail();
-				}
-			}
-				// fix confirmation
-			if ($customer->getConfirmation()) {
-				try {
-					$customer->setConfirmation(null);
-					$customer->save();
-				}catch (Exception $e) {
-				}
-			}
-			Mage::getSingleton('customer/session')->setCustomerAsLoggedIn($customer);
-			$customerHelper->setJsRedirect($customerHelper->_loginPostRedirect());
+			$user_info['provider']="myopenid";
+			$this->handleCustomer($user_info);
 		}else{ 
 			$coreSession->addError('Login failed as you have not granted access.');			
 			$customerHelper->setJsRedirect(Mage::getBaseUrl());
 		}           
     }
+	
+	public function makeCustomerData($user_info) {
+		$data = array();
+
+		$frist_name = $user_info['openid_sreg_nickname'];
+		$last_name = $user_info['openid_sreg_nickname'];
+		$email = $user_info['openid_sreg_email'];
+		
+		$data['provider']=$user_info['provider'];
+		$data['email']=$email;
+		$data['firstname']=$frist_name;
+		$data['lastname']=$last_name;
+		$data['authorId']=$authorId;
+
+		return $data;
+	}	
 }
