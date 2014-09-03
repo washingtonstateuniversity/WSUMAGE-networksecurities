493f852c68cc84105577d62bd236b4b4c8568c4c
jeremyBass <jeremybass@cableone.net>
2014-09-03 13:24:07 -0700

    sets up myspace to use the newer model

--- app/code/local/Wsu/Networksecurities/Helper/Customer.php
+++ app/code/local/Wsu/Networksecurities/Helper/Customer.php
@@ -195,12 +195,12 @@ class Wsu_Networksecurities_Helper_Customer extends Mage_Core_Helper_Abstract {
 	public function getLiveAppSecret() {
 		return trim(Mage::getStoreConfig('wsu_networksecurities/livelogin/consumer_secret'));
 	}
-	public function getMpConsumerKey() {
+/*	public function getMpConsumerKey() {
 		return trim(Mage::getStoreConfig('wsu_networksecurities/myspacelogin/consumer_key'));
 	}
 	public function getMpConsumerSecret() {
 		return trim(Mage::getStoreConfig('wsu_networksecurities/myspacelogin/consumer_secret'));
-	}
+	}*/
 	public function getAuthUrlFq() {
 		$isSecure = Mage::getStoreConfig('web/secure/use_in_frontend');
 		return $this->_getUrl('sociallogin/fqlogin/login', array('_secure'=>$isSecure, 'auth'=>1));
@@ -209,10 +209,10 @@ class Wsu_Networksecurities_Helper_Customer extends Mage_Core_Helper_Abstract {
 		$isSecure = Mage::getStoreConfig('web/secure/use_in_frontend');
 		return $this->_getUrl('sociallogin/livelogin/login', array('_secure'=>$isSecure, 'auth'=>1));
 	}
-	public function getAuthUrlMp() {
+/*	public function getAuthUrlMp() {
 		$isSecure = Mage::getStoreConfig('web/secure/use_in_frontend');
 		return $this->_getUrl('sociallogin/myspacelogin/login', array('_secure'=>$isSecure, 'auth'=>1));
-	}
+	}*/
 	public function getLinkedConsumerKey() {
 		return trim(Mage::getStoreConfig('wsu_networksecurities/linkedin_login/app_id'));
 	}
