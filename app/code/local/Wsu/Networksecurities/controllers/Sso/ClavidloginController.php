b32c19563ee3004f08610b0719ae6053eaafee16
jeremyBass <jeremybass@cableone.net>
2014-09-03 08:03:19 -0700

    correct the clavid implmentation

--- app/code/local/Wsu/Networksecurities/controllers/Sso/ClavidloginController.php
+++ app/code/local/Wsu/Networksecurities/controllers/Sso/ClavidloginController.php
@@ -10,52 +10,17 @@ class Wsu_Networksecurities_Sso_ClavidloginController extends Mage_Core_Controll
 		$userId = $cal->mode;        
 		$coreSession = Mage::getSingleton('core/session');
 		if(!$userId) {
-            $cal_session = Mage::getModel('wsu_networksecurities/sso_clavidlogin')->setcalIdlogin($aol, $name_blog);
+            $cal_session = Mage::getModel('wsu_networksecurities/sso_clavidlogin')->setcalIdlogin($cal, $name_blog);
             $url = $cal_session->authUrl();
 			$this->_redirectUrl($url);
 		}else{ if (!$cal->validate()) {                
                $coreSession->addError('Login failed as you have not granted access.');			
                $customerHelper->setJsRedirect(Mage::getBaseUrl());
-            }else{ $user_info = $cal->getAttributes();                 
+            }else{ 
+				$user_info = $cal->getAttributes();                 
                 if(count($user_info)) {
-                    $frist_name = $user_info['namePerson/first'];
-                    $last_name = $user_info['namePerson/last'];
-                    $email = $user_info['contact/email'];
-                    if(!$frist_name) {
-                        if($user_info['namePerson/friendly']) {
-                        $frist_name = $user_info['namePerson/friendly'] ;   
-                        }else{ $email = explode("@", $email);
-                            $frist_name = $email['0'];
-                        }                   
-                    }
-
-                    if(!$last_name) {
-                        $last_name = '_cal';
-                    }
-					
-					//get website_id and sote_id of each stores
-					$store_id = Mage::app()->getStore()->getStoreId();//add
-					$website_id = Mage::app()->getStore()->getWebsiteId();//add
-					
-                    $data = array('firstname'=>$frist_name, 'lastname'=>$last_name, 'email'=>$user_info['contact/email']);
-                    $customer = $customerHelper->getCustomerByEmail($data['email'],$website_id );//add edition
-                    if(!$customer || !$customer->getId()) {
-						//Login multisite
-						$customer = $customerHelper->createCustomerMultiWebsite($data, $website_id, $store_id );
-						if (Mage::getStoreConfig('wsu_networksecurities/clavidlogin/is_send_password_to_customer')) {
-							$customer->sendPasswordReminderEmail();
-						}
-                    }
-						// fix confirmation
-					if ($customer->getConfirmation()) {
-						try {
-							$customer->setConfirmation(null);
-							$customer->save();
-						}catch (Exception $e) {
-						}
-					}
-                    Mage::getSingleton('customer/session')->setCustomerAsLoggedIn($customer);
-					$customerHelper->setJsRedirect($customerHelper->_loginPostRedirect());
+					$user_info['provider']="clavid";
+					$this->handleCustomer($user_info);
                 }else{ 
 					$coreSession->addError($this->__('Login failed as you have not granted access.'));
 					$customerHelper->setJsRedirect(Mage::getBaseUrl());
@@ -63,7 +28,7 @@ class Wsu_Networksecurities_Sso_ClavidloginController extends Mage_Core_Controll
             }           
         }
     }
-    
+
     public function setBlockAction() {             
         /*$template =  $this->getLayout()->createBlock('sociallogin/clavidlogin')
                 ->setTemplate('sociallogin/au_cal.phtml')->toHtml();
