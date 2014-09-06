d98eb7bc3f303a573e87811a78bba283036906c7
Jeremy Bass <jeremybass@cableone.net>
2014-09-05 21:50:05 -0700

    remove tests

--- app/code/local/Wsu/Networksecurities/Model/Sso/Googlelogin.php
+++ app/code/local/Wsu/Networksecurities/Model/Sso/Googlelogin.php
@@ -22,11 +22,11 @@ class Wsu_Networksecurities_Model_Sso_Googlelogin extends Wsu_Networksecurities_
 		require_once(Mage::getBaseDir('lib').DS.'Oauth2'.DS.'Google_Client.php');
 
 		$google = new Google_Client;
-		var_dump($google);
+		//var_dump(Mage::getBaseDir('lib').DS.'Oauth2'.DS.'service'.DS.'Google_ServiceResource.php');
 		$google->setClientId($this->getConsumerKey());
 		$google->setClientSecret($this->getConsumerSecret());
 		$google->setRedirectUri($this->getRedirectUri());
-		var_dump($google);die();
+		//var_dump($google);die();
 		return $google;
 	}
 }
